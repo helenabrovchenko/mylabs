0836f3f635849b2afaa423ffd0c1b719e63bc7e7
helenabrovchenko <43200600+helenabrovchenko@users.noreply.github.com>
2018-09-12 12:30:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+﻿function f1() {
+	document.getElementById("card-image").src = "01.png"
+}
+
+function f2() {
+		document.getElementById("card-image").src = "02.png"
+
+}
+
+function f3() {
+		document.getElementById("card-image").src = "03.png"
+
+}
+
+function f4() {
+		document.getElementById("card-image").src = "04.png"
+
+}
+
+function f5() {
+	alert("Привет Мир!");
+}
